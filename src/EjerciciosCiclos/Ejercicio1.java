ced12583cd7c7d69a8c5b107aa5033dc9f6b5c75
Ara <aragomezgd@gmail.com>
2025-04-10 19:50:47 -0300

    update package

--- src/EjerciciosCiclos/Ejercicio1.java
+++ src/EjerciciosCiclos/Ejercicio1.java
@@ -1,8 +1,11 @@
 package EjerciciosCiclos;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Ejercicio1 {
+    public static Logger log =
+            Logger.getLogger(Ejercicio1.class.getName());
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -11,13 +14,14 @@ public class Ejercicio1 {
         int respuesta;
         // System.out.println(numRandom);  <-- testeo de rng para verificar
 
-        System.out.println("Adivina el número del 1 al 10");
+        log.info("Adivina el número del 1 al 10");
         do{
             respuesta = sc.nextInt();
             if (respuesta != numRandom){
-            System.out.println("Incorrecto, inténtelo de nuevo");}
+            log.info("Incorrecto, inténtelo de nuevo");}
         }while(respuesta != numRandom);
-        System.out.println("¡Has adivinado el número!");
-        System.out.println("El número es " + numRandom);
+
+        log.info("¡Has adivinado el número!");
+        log.info("El número es " + numRandom);
     }
 }
--- src/EjerciciosCiclos/Ejercicio2.java
+++ src/EjerciciosCiclos/Ejercicio2.java
@@ -6,27 +6,33 @@ import java.util.logging.Logger;
 public class Ejercicio2 {
     public static Logger log =
             Logger.getLogger(Ejercicio2.class.getName());
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String userCorrecto = admin;
+        String userCorrecto = "admin";
         String user;
-        String contraseñaCorrecta = ciclos;
+        String contraseñaCorrecta = "ciclos";
         String contraseña;
         int intentos = 0;
+        boolean usuario = false;
 
-
-        while (intentos < 3){
+        while (intentos < 3 && !usuario) {
             intentos++;
             log.info("Ingrese su usuario");
             user = sc.next();
             log.info("Ingrese su contraseña");
             contraseña = sc.next();
-            if (user.equals(userCorrecto) && contraseña.equals(contraseñaCorrecta)){
+
+            if (user.equals(userCorrecto) && contraseña.equals(contraseñaCorrecta)) {
+                usuario = true;
                 log.info("Has ingresado correctamente");
-            }else{
+            } else {
                 log.info("Las credenciales no son correctas");
             }
-        log.info("Su cuenta ha sido bloqueada");
+        }if (!usuario) {
+            log.warning("Has alcanzado tu máximo número de intentos");
+            log.severe("Su cuenta ha sido bloqueada");
+        }
     }
 }
