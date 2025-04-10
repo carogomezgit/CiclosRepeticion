67f975484f2f8834953c7c16f17a767b8716f165
Ara <aragomezgd@gmail.com>
2025-04-10 18:32:46 -0300

    primer commit

---
+++ src/Ejercicio1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numRandom = (int)(Math.random() * 10 + 1);
+        int respuesta;
+        // System.out.println(numRandom);  <-- testeo de rng para verificar
+
+        System.out.println("Adivina el número del 1 al 10");
+        do{
+            respuesta = sc.nextInt();
+            if (respuesta != numRandom){
+            System.out.println("Incorrecto, inténtelo de nuevo");}
+        }while(respuesta != numRandom);
+        System.out.println("¡Has adivinado el número!");
+        System.out.println("El número es " + numRandom);
+    }
+}
